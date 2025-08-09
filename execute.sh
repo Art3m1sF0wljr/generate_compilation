396c2c924a0777db2ed3705b180a88ede87efc40
dcazacu <158522530+Art3m1sF0wljr@users.noreply.github.com>
2025-08-09 21:29:58 +0200

    Create execute.sh

---
+++ execute.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#python3 -m venv myenv
+source /home/art3m1sf0wl/program/generatecompilation/myenv/bin/activate
+# Activate the virtual environment
+#source myenv/bin/activate
+
+# Install librosa in the virtual environment
+python3 /home/art3m1sf0wl/program/generatecompilation/app.py
